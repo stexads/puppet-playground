7961786bec68b2b20c7e480ddba5f5ae8c5caa01
Stefano Longo <stefano@exads.com>
2025-09-09 16:17:26 +0100

    refactored

--- puppet-environments/production/manifests/nodes/stefano.pp
+++ puppet-environments/production/manifests/nodes/stefano.pp
@@ -2,38 +2,14 @@ class nodes::stefano (
   String $version = '0.0.1'
 ) {
 
-  notify { "Hello, I am node '${::hostname}'": }
-  notify { "This message is coming from <production>/manifests/nodes/stefano.pp": }
-
-
-  ##### MOTD module ######
-  # If we include 'include motd' here, the module will use the template to generate the
-  # /etc/motd file.
-  include motd
-  # Alternativel, we can define the class here and pass out parameters:
-  #class { 'motd':
-  #  content => "Welcome to stefano's node...\n"
-  #}
-
-  # Add a user 'stefano' to the system:
-  user { 'stefano':
-    ensure => 'present',
-    # to remove the user:
-    #ensure => 'absent',
-    #system => true,
-    shell => '/bin/bash',
-    home => '/home/stefano',
-    managehome => true,
-    # following does not work (at least with this Puppet version)
-    #managehome => $ensure ? { present => true, default => false, },
-  }
-
-#  include geoipupdate
-
-  class { 'geoipupdate':
-    account_id  => "123456",
-    license_key => "0987654321",
-    edition_ids => [ "test0", "test1" ],
-  }
+  include geoipupdate
+#  class { 'geoipupdate':
+#    account_id     => "123456",
+#    license_key    => "0987654321",
+#    edition_ids    => [ "test0", "test1" ],
+#    package_ensure => "present",
+##    package_ensure => "absent",
+#    package_name   => 'geoipupdate',
+#  }
 
 }
