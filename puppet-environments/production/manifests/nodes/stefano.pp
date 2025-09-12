b0b98dab23c027f954b6dc1167e29606e88871ec
Stefano Longo <stefano@exads.com>
2025-09-12 12:16:28 +0100

    Added correct r10k environment

--- puppet-environments/production/manifests/nodes/stefano.pp
+++ puppet-environments/production/manifests/nodes/stefano.pp
@@ -2,6 +2,16 @@ class nodes::stefano (
   String $version = '0.0.1'
 ) {
 
-  include geoipupdate
+#  include geoipupdate
+
+  class { 'geoipupdate':
+    account_id         => 'test',
+    license_key        => 'test',
+    edition_ids        => 'GeoIP2-Country GeoIP2-City GeoIP2-ISP GeoIP2-Connection-Type',
+    package_ensure     => 'present',
+    package_name       => 'geoipupdate',
+    service_update_cmd => '/usr/bin/dnf -y update geoipupdate',
+    timer_oncalendar   => 'Mon-Fri *:*:00',
+  }
 
 }
