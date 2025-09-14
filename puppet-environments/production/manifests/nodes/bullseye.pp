6212132d0fbeec33f66c6c69663a9ba99d4d93ef
Stefano Longo <stefano@exads.com>
2025-09-14 23:22:20 +0100

    updated code

---
+++ puppet-environments/production/manifests/nodes/bullseye.pp
@@ -0,0 +1,18 @@
+class nodes::bullseye (
+  String $version = '0.0.1'
+) {
+
+  # For use with hieradata
+  #include geoipupdate
+
+  # For module version >= 0.9.5
+  class { 'geoipupdate':
+    package_ensure     => 'present',
+    account_id         => 'test',
+    license_key        => 'test',
+    edition_ids        => 'GeoIP2-Country GeoIP2-City GeoIP2-ISP GeoIP2-Connection-Type',
+    conf_dir           => '/etc',
+    timer_oncalendar   => '*:*:00/30',
+  }
+
+}
--- puppet-environments/production/manifests/site.pp
+++ puppet-environments/production/manifests/site.pp
@@ -1,9 +1,10 @@
 # Node definitions
 node /\.0x3e.(lan|net)$/ {
+  include "nodes::${hostname}"
 }
 
 # Define your node here...
 # The following line links the file name manifests/node/stefano.pp
-node stefano.0x3e.lan {
-  include "::nodes::${hostname}"
-}
+#node stefano.0x3e.lan {
+#  include "::nodes::${hostname}"
+#}
